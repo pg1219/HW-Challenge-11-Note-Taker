1f16fb027799f92a8e68a52ffc511410d43c6b9c
pg1219 <peter.gronseth@gmail.com>
2023-05-08 23:42:47 -0400

    monday night push

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ const id = Math.floor((1 + Math.random()) * 0x10000)
 .toString(16)
 .substring(1);
 console.log(id)
-
 const app = express();  
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
 app.get('./api/notes', (req, res) => {
-    
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.get('./api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 })
