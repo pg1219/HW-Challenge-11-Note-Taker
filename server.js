22184024fedf7e250d35232f24edcd818b133ee9
pg1219 <peter.gronseth@gmail.com>
2023-05-13 20:19:31 -0400

    bcs push

--- server.js
+++ server.js
@@ -18,32 +18,32 @@ app.get("./api/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "../db/db.json"));
 });
 
-// app.post("/api/notes", (req, res) => {
-//   const { title, text } = req.body;
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
 
-//   if (title && text) {
-//     const newNote = {
-//       title,
-//       text,
-//       note_id: id,
-//     };
-//     const response = {
-//         status: 'success',
-//         body: newNote
-//     }
-//     console.log(response)
-//     res.status(201).json(response)
-//   }else {
-//     res.status(500).json('Error in posting review');
-//   }
-// });
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      note_id: id,
+    };
+    const response = {
+        status: 'success',
+        body: newNote
+    }
+    console.log(response)
+    res.status(201).json(response)
+  }else {
+    res.status(500).json('Error in posting review');
+  }
+});
 
 app.get("./notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.sendFile(path.join(__dirname, "/notes.html"));
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 app.listen(PORT, () => console.log("listening at http://localhost:${PORT}"));
