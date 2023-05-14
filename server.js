208f3691ea7a742904cc4587c3669ec9a02b4e6c
pg1219 <peter.gronseth@gmail.com>
2023-05-14 18:17:48 -0400

    heroku edit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const notes = require("./db/db.json");
 console.log(notes);
 const express = require("express");
 const path = require("path");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const uuid = require("uuid");
 const app = express();
 
